d00a7b96474b011de37ca7975228af3ca45d62e2
Yash Londhe <99034957+yashlondhe90960@users.noreply.github.com>
2023-02-26 23:43:29 +0530

    updated routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -4,3 +4,10 @@ class MyRoutes {
   static String homeDetailsRoute = "/detail";
   static String cartRoute = "/cart";
 }
+
+
+//=====================================================
+//  static String homeRoute = "/home";
+//   static String homeDetailsRoute = "/detail";
+//   static String cartRoute = "/cart";
+======================================================//
