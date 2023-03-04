a6205b839647235d2b33dab50e051d95e0e106e0
Yash Londhe <yashlondhe9151@gmail.com>
2023-03-04 22:04:29 +0530

    Day 22 completed!

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -8,11 +8,67 @@ class CartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.canvasColor,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: "                    Cart".text.color(Colors.white).make(),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: context.canvasColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: "                    Cart".text.color(Colors.white).make(),
+        ),
+        body: Column(
+          children: [
+            _CartList().p32().expand(),
+            Divider(),
+            _CartTotal(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // ignore: deprecated_member_use
+          "\$9999".text.xl5.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+                backgroundColor:
+                    // ignore: deprecated_member_use
+                    MaterialStateProperty.all(context.theme.accentColor)),
+            child: "Buy".text.white.make(),
+          ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  @override
+  __CartListState createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: "Item 1".text.make(),
       ),
     );
   }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,47 +1,90 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:velocity_x/velocity_x.dart';
+
+// class MyTheme {
+//   static ThemeData lightTheme(BuildContext context) => ThemeData(
+//       fontFamily: GoogleFonts.poppins().fontFamily,
+//       cardColor: Colors.white,
+//       canvasColor: creamColor,
+//       buttonTheme: ButtonThemeData(buttonColor: darkBluishColor),
+//       appBarTheme: AppBarTheme(
+//         color: Colors.white,
+//         elevation: 0.0,
+//         iconTheme: const IconThemeData(color: Colors.black),
+//         // ignore: deprecated_member_use
+//         textTheme: Theme.of(context).textTheme,
+//       ),
+//       colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
+//           .copyWith(secondary: darkBluishColor));
+
+//   static ThemeData darkTheme(BuildContext context) => ThemeData(
+//       fontFamily: GoogleFonts.poppins().fontFamily,
+//       cardColor: Colors.black,
+//       canvasColor: darkCreamColor,
+//       buttonTheme: ButtonThemeData(buttonColor: lightBluishColor),
+//       appBarTheme: AppBarTheme(
+//         color: Colors.black,
+//         elevation: 0.0,
+//         iconTheme: const IconThemeData(color: Colors.white),
+
+//         // titleTextStyle: TextStyle(
+//         //   color: Colors.white,
+//         // ),
+//         // ignore: deprecated_member_use
+//         textTheme: Theme.of(context).textTheme.copyWith(
+//             headline6:
+//                 context.textTheme.headline6!.copyWith(color: Colors.white)),
+//       ),
+//       colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
+//           .copyWith(secondary: darkBluishColor));
+
+//   // colors
+//   static Color creamColor = Color(0xfff5f5f5);
+//   static Color darkCreamColor = Vx.gray900;
+//   static Color darkBluishColor = Color.fromARGB(255, 50, 105, 223);
+//   static Color lightBluishColor = Vx.indigo500;
+// }
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.deepPurple,
       fontFamily: GoogleFonts.poppins().fontFamily,
       cardColor: Colors.white,
       canvasColor: creamColor,
-      buttonTheme: ButtonThemeData(buttonColor: darkBluishColor),
+      buttonColor: darkBluishColor,
+      accentColor: darkBluishColor,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
-        iconTheme: const IconThemeData(color: Colors.black),
-        // ignore: deprecated_member_use
+        iconTheme: IconThemeData(color: Colors.black),
         textTheme: Theme.of(context).textTheme,
-      ),
-      colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
-          .copyWith(secondary: darkBluishColor));
+      ));
 
   static ThemeData darkTheme(BuildContext context) => ThemeData(
+      brightness: Brightness.dark,
       fontFamily: GoogleFonts.poppins().fontFamily,
       cardColor: Colors.black,
       canvasColor: darkCreamColor,
-      buttonTheme: ButtonThemeData(buttonColor: lightBluishColor),
+      buttonColor: lightBluishColor,
+      accentColor: Colors.white,
       appBarTheme: AppBarTheme(
         color: Colors.black,
         elevation: 0.0,
-        iconTheme: const IconThemeData(color: Colors.white),
-
-        // titleTextStyle: TextStyle(
-        //   color: Colors.white,
-        // ),
-        // ignore: deprecated_member_use
+        iconTheme: IconThemeData(color: Colors.white),
         textTheme: Theme.of(context).textTheme.copyWith(
-            headline6:
-                context.textTheme.headline6!.copyWith(color: Colors.white)),
-      ),
-      colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
-          .copyWith(secondary: darkBluishColor));
+              headline6:
+                  context.textTheme.headline6?.copyWith(color: Colors.white),
+            ),
+      ));
 
-  // colors
+  //Colors
   static Color creamColor = Color(0xfff5f5f5);
   static Color darkCreamColor = Vx.gray900;
-  static Color darkBluishColor = Color.fromARGB(255, 50, 105, 223);
+  static Color darkBluishColor = Color(0xff403b58);
   static Color lightBluishColor = Vx.indigo500;
 }
